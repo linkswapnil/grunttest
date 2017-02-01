07632b40628ff0e62f0865af5d0c95e71a384513
linkswapnil <Shagun123>
2017-02-01 18:46:10 +0530

    boilerplate

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,11 @@ module.exports = function(grunt) {
         grunt.initConfig({
             jasmine : {
                 // Your project's source files
-                src : 'js/*.js',
+                src : ['js/*.js', '!js/*.spec.js'],
                 // Your Jasmine spec files
 
                 options : {
-                    specs : 'js/*spec.js'
+                    specs : 'js/*.spec.js'
                 }
             }
         });
--- js/MyModule.spec.js
+++ js/MyModule.spec.js
@@ -1,6 +1,16 @@
 describe("My module", function() {
     it("answers to myMethod() correctly", function() {
         var instance = new MyModule();
+        console.log("In");
+        expect(instance.myMethod()).toEqual("Hello    world");
+        expect(instance.myMethod()).toEqual("Hello world");
+        expect(instance.myMethod()).toEqual("Hello world");
+    });
+
+    it("answers to myMethod() correctly", function() {
+        var instance = new MyModule();
+        expect(instance.myMethod()).toEqual("Hello world");
+        expect(instance.myMethod()).toEqual("Hello world");
         expect(instance.myMethod()).toEqual("Hello world");
     });
 });
